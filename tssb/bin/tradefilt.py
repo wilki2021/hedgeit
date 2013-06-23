a9114e08a13d622bab408d01d903d6a080534889
Bob Wilkinson <bwilkinson20@gmail.com>
2013-06-23 12:03:53 -0500

    enhance --rescan mode of tradefilt.py

--- tssb/bin/tradefilt.py
+++ tssb/bin/tradefilt.py
@@ -14,6 +14,7 @@ import sys
 import json
 import numpy
 import copy
+import time
 
 class SimMain(object):
     
@@ -36,6 +37,7 @@ class SimMain(object):
         self._with_val = False
         self._var_thresh = 3.0
         self._varbase = 'db/ALL'
+        self._headers = False
 
     def main(self,argv=None):
         try:
@@ -96,6 +98,10 @@ class SimMain(object):
                 self.usage()
                 sys.exit(1)
             
+        if rescan:
+            self.do_rescan(args)
+            return
+            
         if len(args) != 3:
             print 'Not enough arguments to tradefilt!'
             self.usage()
@@ -116,17 +122,109 @@ class SimMain(object):
             vars_ = VarParser('%s.TXT' % self._varbase)
             if not os.path.exists(runname):
                 os.mkdir(runname)
-        os.chdir(runname)
     
+        os.chdir(runname)
+        self.run_years(yearstart, yearend, rescan=False, vars_=vars_)        
+        os.chdir('..')
+        
+    def usage(self):
+        print '''
+usage: tradefilt.py [options] <run-name> <year-start> <year-end>
+       tradefile.py --rescan [run-directories]
+
+    Performs an "outer" walk-forward analysis loop across a series of
+    years per the command-line arguments.  Each "inner" walk-forward 
+    is a discrete (and currently hard-coded) series of steps to select
+    a smaller indicator set from a larger one and then select and train
+    a series of models from them.
+    
+    Parameters:
+        <run-name>    - this is the name of the subdirectory that will
+                        contain run results.  The run directory will 
+                        always have one sub-directory for each year in 
+                        the outer walk-forward loop.
+        <year-start>  - integer, year to start the outer walk-forward.
+                        NOTE - for any given walk-forward year, that
+                        year is included in the training set and the 
+                        subsequent year is treated as the test/validation
+                        period.
+        <year-end>    - integer, year to end the outer walk forward.
+
+    Options:
+        --rescan      - do not perform any new TSSB runs.  Takes an optional
+                        list of run directories to scan - by default it 
+                        assumes all subdirectories in the current directory
+                        are runs and will rescan each.  If this option is
+                        used it is the only option that is processed.
+        --trades <file>- file containing the trades to use for this
+                        model run. (default = tssp_long.csv)
+        --num_vars <num>- number of variables to use for each source
+                        model. (default = 1)
+        --model <type>- model type to use for source models.  Supported
+                        options: LINREG, QUADRATIC, GRNN. (default =
+                        LINREG)
+        --with_val    - use a validation year to select models based
+                        on out-of-sample performance.  Note - this option
+                        shifts the true walk-forward year ahead by 1 so
+                        performance for 2002 is actually out-of-sample
+                        performance for 2004 
+        --retention <num>-value to use for the STEPWISE RETENTION 
+                        setting in all relevant stages
+        --min_crit <float>-value to use for the MIN CRITERION FRACTION
+                        setting in all relevant stages
+        --var_thresh <float>-threshold to use for variable selection after
+                        the subsampling stage
+        --vars <base> - base name of the aggregate variable database and
+                        corresponding definition file 
+        --stage1_crit <crit>-set the CRITERION to be used for the stage 1-
+                        subsampling phase used for variable down-select. 
+                        Most be one of the supported TSSB criterion strings.
+        --stage2_crit <crit>-set the CRITERION to be used for the stage 2-
+                        find groups phase used for model selection. 
+                        Most be one of the supported TSSB criterion strings.
+        --stage3_crit <crit>-set the CRITERION to be used for the stage 3-
+                        walk-forward testing.
+'''
+    
+    def do_rescan(self,dirs):
+        # if dirs is not empty then we assume user provided a list they
+        # wanted to rescan.  If no list is provided then we scan all
+        if not len(dirs):
+            dirents = os.listdir('.')
+            for ent in dirents:
+                if os.path.isdir(ent) and ent != '..' and ent != '.' and ent != 'db':
+                    dirs.append(ent)
+                    
+        for d in sorted(dirs):
+            os.chdir(d)
+            print 'INFO: scanning %s' % d
+            if os.path.exists('runvars.json'):
+                for line in open('runvars.json').readlines():
+                    print line.strip()
+            # inside a run directory we should find one directory per year that
+            # the filter was run for
+            yearstart = 9999
+            yearend = 1
+            for ent in os.listdir('.'):
+                if os.path.isdir(ent) and ent != '..' and ent != '.':
+                    y = int(ent)
+                    if y < yearstart:
+                        yearstart = y
+                    if y > yearend:
+                        yearend = y
+                    
+            self.run_years(yearstart, yearend, rescan=True)
+            os.chdir('..')
+
+    def run_years(self, yearstart, yearend, rescan=False, vars_=None):
         if not rescan:
             # only dump out the runvars if not rescanning
             wf = open("runvars.json",'w')
             vardump = json.dumps(self._varmap,indent=4,sort_keys=True)
             wf.write(vardump)
             wf.close()
-        
+
         wf = open("perf.csv","w")
-        headers = False
         resultsumm = {}
         for y in range(yearstart, yearend+1):
             # sanity check the year the user gave us to make sure it makes sense
@@ -141,8 +239,8 @@ class SimMain(object):
             else:
                 res = self.run_iteration(y, vars_, 13)
     
-            if not headers:
-                headers = True
+            if not self._headers:
+                self._headers = True
                 line = 'year'
                 for model in sorted(res.tssbrun().walkforward_summ().iterkeys()):
                     line = line + ",%s" % model
@@ -201,65 +299,7 @@ class SimMain(object):
                                                          numpy.average(v[0]),
                                                          numpy.average(v[1]) / numpy.std(v[1]),
                                                          numpy.sum(v[4]) * 1.0 / numpy.sum(v[3]))
-        
-        os.chdir('..')
-    
-    def usage(self):
-        print '''
-usage: tradefilt.py [options] <run-name> <year-start> <year-end>
-
-    Performs an "outer" walk-forward analysis loop across a series of
-    years per the command-line arguments.  Each "inner" walk-forward 
-    is a discrete (and currently hard-coded) series of steps to select
-    a smaller indicator set from a larger one and then select and train
-    a series of models from them.
-    
-    Parameters:
-        <run-name>    - this is the name of the subdirectory that will
-                        contain run results.  The run directory will 
-                        always have one sub-directory for each year in 
-                        the outer walk-forward loop.
-        <year-start>  - integer, year to start the outer walk-forward.
-                        NOTE - for any given walk-forward year, that
-                        year is included in the training set and the 
-                        subsequent year is treated as the test/validation
-                        period.
-        <year-end>    - integer, year to end the outer walk forward.
-
-    Options:
-        --rescan      - do not perform any new TSSB runs, just uses 
-                        existing .log files from a previous run and 
-                        re-reports results
-        --trades <file>- file containing the trades to use for this
-                        model run. (default = tssp_long.csv)
-        --num_vars <num>- number of variables to use for each source
-                        model. (default = 1)
-        --model <type>- model type to use for source models.  Supported
-                        options: LINREG, QUADRATIC, GRNN. (default =
-                        LINREG)
-        --with_val    - use a validation year to select models based
-                        on out-of-sample performance.  Note - this option
-                        shifts the true walk-forward year ahead by 1 so
-                        performance for 2002 is actually out-of-sample
-                        performance for 2004 
-        --retention <num>-value to use for the STEPWISE RETENTION 
-                        setting in all relevant stages
-        --min_crit <float>-value to use for the MIN CRITERION FRACTION
-                        setting in all relevant stages
-        --var_thresh <float>-threshold to use for variable selection after
-                        the subsampling stage
-        --vars <base> - base name of the aggregate variable database and
-                        corresponding definition file 
-        --stage1_crit <crit>-set the CRITERION to be used for the stage 1-
-                        subsampling phase used for variable down-select. 
-                        Most be one of the supported TSSB criterion strings.
-        --stage2_crit <crit>-set the CRITERION to be used for the stage 2-
-                        find groups phase used for model selection. 
-                        Most be one of the supported TSSB criterion strings.
-        --stage3_crit <crit>-set the CRITERION to be used for the stage 3-
-                        walk-forward testing.
-'''
-    
+            
     def apply_script_template(self, template, output, varmap):
         assert(os.path.exists(template))
         if os.path.exists(output):
@@ -320,7 +360,17 @@ usage: tradefilt.py [options] <run-name> <year-start> <year-end>
         self._varmap['<APPEND_DATABASES>'] = app_dbs
         self.apply_script_template(os.path.join("..","..",'createdb.txt'), 'createdb.txt', self._varmap)
         log = 'create_audit.log'
-        self.run_tssb_wrapper('createdb.txt',log)            
+        retry_cnt = 0
+        while retry_cnt < 3:
+            try:
+                # don't understand this, but dealing with periodic failures of 
+                # this step in the iteration - most often several years in
+                self.run_tssb_wrapper('createdb.txt',log)
+                break
+            except:
+                ++retry_cnt
+                print 'tssb createdb.txt failed, trying %d more times' % (3-retry_cnt)
+                time.sleep(5.0)            
             
         self._varmap['<VAR_1>'] = varlist[0][0]
         self._varmap['<VAR_N>'] = varlist[-1][0]
@@ -377,7 +427,7 @@ usage: tradefilt.py [options] <run-name> <year-start> <year-end>
         return ret
     
     def rescan_iteration(self,year):
-        if self._with_val:
+        if os.path.exists('%d/pselect_test_audit.log' % year):
             audfile = '%d/pselect_test_audit.log' % year
         else:
             audfile = '%d/pselect_audit.log' % year
